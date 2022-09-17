51353365b02214fd0a8433eeb7daa04269868b6f
startender <mathsitedu@mail.ru>
2022-09-17 13:54:12 +0300

    pulling data

--- delivery-rn-app/components/Categories.js
+++ delivery-rn-app/components/Categories.js
@@ -1,8 +1,23 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import CategoryCard from "./CategoryCard";
+import sanityClient, { urlFor } from '../sanity'
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+        *[_type == "category"]
+        `
+      )
+      .then((data) => {
+        setCategories(data);
+      })
+  }, [])
+
   return (
     <ScrollView
       contentContainerStyle={{
@@ -13,6 +28,14 @@ const Categories = () => {
       showsHorizontalScrollIndicator={false}
     >
       {/* CategoryCard */}
+
+      {categories.map((category) => {
+        <CategoryCard
+        key={category._id}
+        imgUrl={urlFor(category.image).width(200).url()}
+        title={category.name}
+        />
+      })}
 
       <CategoryCard imgUrl="https://links.papareact.com/gn7" title="123" />
       <CategoryCard imgUrl="https://links.papareact.com/gn7" title="124" />
